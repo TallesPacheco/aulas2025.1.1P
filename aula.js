df86f7ff4212b0d7b304404dda0c0d1c1c43e6f7
TallesPacheco <tallespacheco@omnus.com.br>
2025-05-07 19:08:13 -0300

    tudo

---
+++ aula.js
@@ -0,0 +1,12 @@
+
+
+var nome = "Talles";
+var segundoNome ="Vitoria";
+let variavelSemValor;
+var temp = nome;
+
+nome = segundoNome;
+segundoNome = temp  ;
+
+console.log(nome);
+console.log(segundoNome);
---
+++ aula2.js
@@ -0,0 +1,86 @@
+// bloco de codigo
+{
+    {
+    {}
+        console.log("Olá");
+        console.log('Mundo');
+    }
+}
+
+// comentarios
+// Comentário de uma única linha
+console.log('Linha 1');
+
+/* Comentário de
+    multiplas linhas 
+    aqui tb é comentario
+   múltiplas linhas 
+*/
+console.log('Linha 2');
+
+
+
+
+
+
+
+// Tipagem Fraca
+
+let qualquer = 'Legal';
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.15;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+
+// Strings
+
+
+const escola = 'Aesa-Cesa';
+console.log(escola.charAt(4));
+console.log(escola.substring(1));
+console.log('Escola '.concat(escola).concat('!'));
+console.log('Ana, Maria, Pedro'.split(','));
+
+
+
+
+// BOOLEANS
+
+let isAtivo = true;
+console.log(isAtivo);
+
+let notActive = false;
+console.log(notActive);
+console.log(!notActive);
+console.log(!isAtivo);
+
+
+
+
+// Números aritiméticos
+
+let idade = 20;         // número inteiro
+let peso = 65.5;        // número decimal
+
+
+
+let valor1 = 10; 
+let valor2 = 5;
+console.log(valor1 + valor2); // Soma → 15
+console.log(valor1 - valor2); // Subtração → 5
+console.log(valor1 * valor2); // Multiplicação → 50
+console.log(valor1 / valor2); // Divisão → 2 
+
+
+// https://playcode.io/javascript
+
+
+
+
+
+
+
+
